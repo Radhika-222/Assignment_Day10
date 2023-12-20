3607335f148d640893a9460dd8b924a3016c626c
Radhika-222 <wandareradhika@gmail.com>
2023-12-20 17:45:33 +0530

    Assignment

---
+++ Assignment_Day10/Assignment_Day10/ProductRegistration.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_Day10
+{
+    public partial class ProductRegistration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            LblMsg.Visible = false;
+        }
+
+        protected void BtnRegister_Click(object sender, EventArgs e)
+        {
+            LblMsg.Visible = true;
+            LblMsg.Text = "Product Name: " + TextBox1.Text;
+            LblMsg.Text += "<br/> Product Category: " + DdlCategory.Text;
+            LblMsg.Text += "<br/> Product Price: " + TxtPrice.Text;
+            LblMsg.Text += "<br/> Product Description: " + TxtDesc.Text;
+            LblMsg.Text += "<br/> Release Date: " + CalRelDate.SelectedDate.ToShortDateString();
+            LblMsg.Text += "<br/> SuccessFul Registration!!!";
+        }
+    }
+}
